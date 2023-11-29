ab7d32fcf49499d302f353734ad164e2b0dcf705
rodrigomonney <rodrigo.monney@gmail.com>
2023-11-29 03:41:48 -0300

    Add: skeleton screen for loading home.

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -1,7 +1,7 @@
 import { Header } from '@/components/header';
 import { ReactNode } from 'react';
 
-export default function StoreLayout({ children }: { children: ReactNode }) {
+export default async function StoreLayout({ children }: { children: ReactNode }) {
     return (
         <div className='mx-auto grid min-h-screen w-full max-w-[1600px] grid-rows-app gap-5 p-8'>
             <Header />
